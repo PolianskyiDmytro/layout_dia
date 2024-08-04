2a164cc0781479e87b613852b10ef69c94fae55a
Polianskyi Dmytro <dimdemon1@gmail.com>
2024-08-04 16:16:40 +0300

    make build

---
+++ index.b6197c2a.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".dropdown");o.querySelector(".dropdown__icon").addEventListener("click",()=>{o.classList.contains("dropdown--active")?o.classList.remove("dropdown--active"):o.classList.add("dropdown--active")});
+//# sourceMappingURL=index.b6197c2a.js.map
